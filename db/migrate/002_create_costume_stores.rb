6d04bd813708659feb74a90c2209e554cd353d6f
Maverick Tesini <mavericktesini@gmail.com>
2018-07-01 23:01:29 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,11 +3,11 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
-      t.string :location 
+      t.string :location
       t.integer :costume_inventory
-      t.integer :employees_count 
-      t.timestamps 
-    end 
-  end 
-  
+      t.integer :employees_count
+      t.timestamps
+    end
+  end
+
 end
